df667c78e08d8cbe2428b4cba21a497d5f5b970b
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2026-05-08 00:12:09 -0400

    face landmarking

---
+++ face_detect.py
@@ -0,0 +1,48 @@
+import os
+
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+mod_path = "face_detector.tflite"
+
+if not os.path.exists(mod_path):
+    print(f"Downloading model...")
+    os.system(
+        f"wget -q -O {mod_path} https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite"
+    )
+base_options = python.BaseOptions(model_asset_path=mod_path)
+options = vision.FaceDetectorOptions(base_options=base_options)
+detector = vision.FaceDetector.create_from_options(options)
+
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        break
+
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
+
+    # Detect here
+    detection_result = detector.detect(mp_img)
+
+    # Visualization
+    if detection_result.detections:
+        for detections in detection_result.detections:
+            bbox = detections.bounding_box
+            start_point = int(bbox.origin_x), int(bbox.origin_y)
+            end_point = int(bbox.origin_x + bbox.width), int(bbox.origin_y + bbox.height)
+            cv2.rectangle(frame, start_point, end_point, (0, 255, 0), 2)
+
+    cv2.imshow("Mediapipe Face detector", cv2.flip(frame, 1))
+
+    # Quit with esc
+    if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from face-thing!")
+
+
+if __name__ == "__main__":
+    main()
