d9cb3c8ba0eac2d239beab0a2c4edd350a0d4f62
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2026-06-02 17:39:22 -0400

    Hiper simplification of serial bridge function

--- serial_bridge.py
+++ serial_bridge.py
@@ -3,42 +3,17 @@ import json
 import serial
 import time
 
-# MediaPipe Face Landmarker (ARKit 52) Blendshape Keys:
-# _neutral, browDownLeft, browDownRight, browInnerUp, browOuterUpLeft, browOuterUpRight,
-# cheekPuff, cheekSquintLeft, cheekSquintRight, eyeBlinkLeft, eyeBlinkRight,
-# eyeLookDownLeft, eyeLookDownRight, eyeLookInLeft, eyeLookInRight, eyeLookOutLeft,
-# eyeLookOutRight, eyeLookUpLeft, eyeLookUpRight, eyeSquintLeft, eyeSquintRight,
-# eyeWideLeft, eyeWideRight, jawForward, jawLeft, jawOpen, jawRight, mouthClose,
-# mouthDimpleLeft, mouthDimpleRight, mouthFrownLeft, mouthFrownRight, mouthFunnel,
-# mouthLeft, mouthLowerDownLeft, mouthLowerDownRight, mouthPressLeft, mouthPressRight,
-# mouthPucker, mouthRight, mouthRollLower, mouthRollUpper, mouthShrugLower,
-# mouthShrugUpper, mouthSmileLeft, mouthSmileRight, mouthStretchLeft, mouthStretchRight,
-# mouthUpperUpLeft, mouthUpperUpRight, noseSneerLeft, noseSneerRight
-
-
-# Solo selecciona cuales mandar. Este es el default sin ojos aun.
-SELECTED_SHAPES = ["jawOpen", "eyeBlinkLeft", "eyeBlinkRight", "mouthSmileLeft", "mouthSmileRight"]
-# Los 8 minimos necesarios para conseguir direccion del ojo.
-# SELECTED_SHAPES = ["eyeLookInLeft", "eyeLookOutLeft", "eyeLookUpLeft", "eyeLookDownLeft",
-#     "eyeLookInRight",
-#     "eyeLookOutRight",
-#     "eyeLookUpRight",
-#     "eyeLookDownRight"]
-
-
-# Sobre la direccion del cuello.
-TRANSFORM_KEYS = [
-    "m00", "m01", "m02", "m03",
-    "m10", "m11", "m12", "m13",
-    "m20", "m21", "m22", "m23",
-]
-
-# Configuracion sobre el puerto y el baudrate.
-# Consideren bajar/subir el baudrate si se tranca.
-# En esp32 este esta excelente, probablemente sea menos en uno.
+# Hiper simplificado. Ahora depende tambien de set_directions.py
+# Hecho con poca verguenza con IA. Puras mamadas.
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)
 last_send_time = 0
 send_interval = 0.2
+
+def to_hex(val):
+    n = round(val)
+    if n < 0:
+        return "-" + format(-n, "x")
+    return format(n, "x")
 
 for line in sys.stdin:
     line = line.strip()
@@ -54,21 +29,7 @@ for line in sys.stdin:
     if current_time - last_send_time < send_interval:
         continue
 
-    blendshapes = data.get("blendshapes", {})
-    transform = data.get("transform", {})
-
-    values = [f"{blendshapes.get(s, 0):.3f}" for s in SELECTED_SHAPES]
-    if transform:
-        values.extend(str(transform.get(k, 0)) for k in TRANSFORM_KEYS)
-
-    # Formato de envio
-    # $v1,v2,...,vm,m00,m01,...,tx,ty,tz#
-    # Un paquete inicia con $
-    # Primero lo primero se manda los valores en SELECTED_SHAPES
-    # Luego se manda los valores en TRANSFORM_KEYS
-    # Se manda primero los valores de blendshapes, luego los de transform, y termina con #
-    packet = "$" + ",".join(values) + "#"
-    # No calcula rotacion de esta transformacion.
-    # Deberia hacer eso...
+    parts = [f"{k}:{to_hex(v)}" for k, v in data.items()]
+    packet = "$" + ";".join(parts) + "#"
     ser.write(packet.encode("utf-8"))
     last_send_time = current_time
